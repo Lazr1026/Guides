9d0ffcf50de8d464e7a9d18eced261b99f877492
Lazr <68634861+Lazr1026@users.noreply.github.com>
2022-03-02 18:13:17 -0800

    Add files via upload

--- files/unbrick/pinand.sh
+++ files/unbrick/pinand.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-wget https://github.com/Lazr1026/Guides/blob/main/files/unbrick/unbrick.zip
-unzip unbrick.zip
+wget https://raw.githubusercontent.com/Lazr1026/Guides/main/files/unbrick/pinand.c
 gcc pinand.c -g -o pinand
 echo "done setting up pinand!"
